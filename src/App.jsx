a54dff44f0827624697edaeeba41bd5542223b41
Ugurb20 <ugurbukcuoglu@gmail.com>
2023-02-26 17:20:03 +0300

    pet cards added with mock data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./App.css";
 import BottomNavBar from "./components/bottom-nav/bottomNav";
+import PetCard from "./components/pet-card/petCard";
+import HomePage from "./pages/home-page/home";
 
 function App() {
   return (
     <div className="App">
-      <BottomNavBar />
+      <div className="wrapper">
+        <Routes>
+          <Route element={<HomePage />} path="/" />
+        </Routes>
+        <BottomNavBar />
+      </div>
     </div>
   );
 }
---
+++ src/pages/home-page/home.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import PetCard from "../../components/pet-card/petCard";
+
+export default function HomePage() {
+  return (
+    <Fragment>
+      <PetCard />
+    </Fragment>
+  );
+}
