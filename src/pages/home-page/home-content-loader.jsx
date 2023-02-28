8fd03f0e133ea6bc2f54ad8f92551f9a0ea1dd0f
Oguz Vuruskaner <ovuruska@outlook.com>
2023-02-28 20:58:24 +0300

    Added content loader.

---
+++ src/pages/home-page/home-content-loader.jsx
@@ -0,0 +1,27 @@
+import ContentLoader from 'react-content-loader'
+import useScreenSize from 'use-screen-size'
+
+const HomeContentLoader = () => {
+    const {width,height} = useScreenSize()
+    return <div style={{
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+
+    }}>
+        <ContentLoader
+            speed={4}
+            width={width}
+            height={height}
+        >
+            <rect width={width / 2 - 80} height={96} rx={8} ry={8} x={40} y={48} />
+            <rect width={width / 2 - 80} height={96}  rx={8} ry={8} radius={8} x={width/2 + 40} y={48} />
+            <rect width={width / 2 - 80} height={72} rx={8} ry={8} x={40} y={180} />
+            <rect width={width / 2 - 80} height={72}  rx={8} ry={8} radius={8} x={width/2 + 40} y={180} />
+        </ContentLoader>
+    </div>
+}
+
+export default HomeContentLoader
--- src/pages/home-page/home.jsx
+++ src/pages/home-page/home.jsx
@@ -1,17 +1,23 @@
-import { Fragment, useEffect } from "react";
+import {Fragment, useEffect} from "react";
 import BookService from "../../components/book-service-card/bookServiceCard";
 import PetCard from "../../components/pet-card/petCard";
 import SubHeading from "../../components/sub-heading/subHeading";
 import useFetchPets from "../../hooks/useFetchPets";
+import HomeContentLoader from "./home-content-loader.jsx";
+
 
 export default function HomePage() {
-  const loading = useFetchPets();
+    const loading = useFetchPets();
 
-  return (
-    <Fragment>
-      <PetCard />
-      <SubHeading title="Book a Service" />
-      <BookService />
+    if (loading)
+    return <Fragment>
+        <HomeContentLoader/>
     </Fragment>
-  );
+
+    return (<Fragment>
+
+            <PetCard/>
+            <SubHeading title="Book a Service"/>
+            <BookService/>
+        </Fragment>);
 }
