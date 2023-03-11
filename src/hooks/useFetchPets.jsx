a27ca8464cdc1be77fb1fa2ccf7293d37b540fd4
Ugurb20 <ugurbukcuoglu@gmail.com>
2023-03-11 13:21:41 +0300

    firebase cd

--- src/hooks/useFetchPets.jsx
+++ src/hooks/useFetchPets.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { flushSync } from "react-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { PetsActions } from "../store/pets-slice";
 
@@ -30,19 +31,22 @@ export default function useFetchPets() {
     if (!window.localStorage.getItem("token")) {
       const resp = await (
         await fetch(
-          "http://54.85.253.78:80/api/auth/customer/login",
+          "https://d3v5l9s7gecmms.cloudfront.net/api/auth/customer/login",
           requestOptions
         )
       ).json();
+      console.log(resp);
       window.localStorage.setItem("token", resp.token);
       dispatch(PetsActions.setId(resp.profile.id));
+    } else {
+      console.log(window.localStorage.getItem("token"));
     }
 
     ///Get Pets
     var myHeaders2 = new Headers();
     myHeaders2.append(
       "Authorization",
-      `Token ${window.localStorage.getItem("token")}`
+      "Token a3c4fbcde633ba8d9d9f344177f7e6935535dde2d8afbf8d815d59fbea8c3e5e"
     );
 
     var requestOptions2 = {
@@ -50,8 +54,9 @@ export default function useFetchPets() {
       headers: myHeaders2,
       redirect: "follow",
     };
+
     const resp2 = await fetch(
-      `http://54.85.253.78:80/api/scheduling/customer/${id}`,
+      `https://d3v5l9s7gecmms.cloudfront.net/api/me`,
       requestOptions2
     );
 
@@ -66,6 +71,7 @@ export default function useFetchPets() {
 
   useEffect(() => {
     dispatch(PetsActions.emptyPets());
+    window.localStorage.removeItem("token", false);
     authAndFetch();
   }, []);
 
