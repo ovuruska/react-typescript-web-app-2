aa5e8194687f53bd40af4e50a7899f156e5e9569
Oguz Vuruskaner <ovuruska@outlook.com>
2023-04-02 15:16:12 +0300

    fix(esModuleInterop)

--- src/components/book/calender/custom-calender.tsx
+++ src/components/book/calender/custom-calender.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from "react";
 import "./custom-calender.css";
 import "../../../App.css";
-import * as moment from "moment";
+import moment from 'moment'
 import {MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight} from "react-icons/md";
 
 interface CustomCalendarProps {
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
+  plugins: [react()], resolve: {
     alias: {
+      '@data': '/src/data',
       '@utils': '/src/utils',
       '@domain': '/src/domain',
       '@features': '/src/features',
       '@components': '/src/components',
       '@hooks': '/src/hooks',
       '@assets': '/src/assets',
+      '@pages': '/src/pages',
 
     },
   },
