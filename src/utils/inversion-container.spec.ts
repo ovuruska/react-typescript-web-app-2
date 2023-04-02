9ec79641c6c7ddbf3f8267b8ea56ff5181ae469c
Oguz Vuruskaner <ovuruska@outlook.com>
2023-04-02 10:18:02 +0300

    chore(Husky)

--- src/utils/inversion-container.spec.ts
+++
@@ -1,9 +0,0 @@
-import {getContainer} from "@utils/inversion-container";
-
-describe("Inversion container", () => {
-  it("should create a container", () => {
-    const container = getContainer();
-    expect(container).toBeDefined();
-  });
-
-});
