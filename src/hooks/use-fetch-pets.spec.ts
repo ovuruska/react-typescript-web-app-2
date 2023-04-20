8fdaa401917483a6aca9c05843cf4f87bd906db6
Oguz Vuruskaner <ovuruska@outlook.com>
2023-04-20 19:50:38 +0300

    feat(month picker)

--- src/hooks/use-fetch-pets.spec.ts
+++
@@ -1,8 +0,0 @@
-import useFetchPets from '@hooks/use-fetch-pets';
-
-describe('useAvailableSlots', () => {
-  it('should be defined.', () => {
-    expect(useFetchPets).toBeDefined();
-  });
-  // TODO: implement more tests.
-});
